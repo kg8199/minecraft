8ed1531c1d0b0f1922abfa4db1e1eef8ca948782
kamilgeagea <kamilgeagea8199@gmail.com>
2022-03-19 14:10:02 -0400

    added block class

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,10 @@
  * File where we store the constants
  */
 
+// Camera
 export const CAMERA_FIELD_OF_VIEW = 75;
 export const CAMERA_MIN_DISTANCE = 0.1;
 export const CAMERA_MAX_DISTANCE = 1000;
+
+// Blocks
+export const BLOCK_SIZE = 5;
---
+++ src/models/Block.ts
@@ -0,0 +1,59 @@
+import {
+  Scene,
+  BoxBufferGeometry,
+  MeshBasicMaterial,
+  Mesh,
+  EdgesGeometry,
+  LineSegments,
+  LineBasicMaterial,
+} from "three";
+
+import { BLOCK_SIZE } from "../constants";
+
+/**
+ * The Block class builds the squared object that we see in the game (example: cube of dirt, cube of sand, water...)
+ */
+
+class Block {
+  // Coordinates of the block
+  x: number;
+  y: number;
+  z: number;
+
+  constructor(x: number, y: number, z: number) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+
+  // Method that displays the block
+  display = (scene: Scene) => {
+    // Create the block
+    const blockBox = new BoxBufferGeometry(BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE); // Create a new box (width, height, depth)
+    const blockMesh = new MeshBasicMaterial({ color: 0x00ff00 }); // What is displayed on the block (the color / image)
+    let block = new Mesh(blockBox, blockMesh); // Combine the block and the material
+
+    // Position the block
+    block.position.x = this.x;
+    block.position.y = this.y;
+    block.position.z = this.z;
+
+    // Create edges for the block (borders)
+    const edges = new EdgesGeometry(blockBox); // Position the border
+    let line = new LineSegments(
+      edges, // Position
+      new LineBasicMaterial({ color: 0xffffff }) // Material
+    ); // Create the border
+
+    // Position the edges
+    line.position.x = this.x;
+    line.position.y = this.y;
+    line.position.z = this.z;
+
+    // Display the block
+    scene.add(line);
+    scene.add(block); // Add the block to the scene (our environement)
+  };
+}
+
+export default Block;
